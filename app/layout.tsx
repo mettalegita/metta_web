0395c9fd52b065e85de498a7a88575954bb1f625
Mirzalazuardi H <mirzalazuardi@gmail.com>
2026-02-28 03:42:43 +0700

    Add Google Analytics gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { satoshi } from '@/utils/fonts'
 import { ThemeModeProvider } from '@/utils/Providers'
 import type { Metadata } from 'next'
 import { ReactNode, Suspense } from 'react'
+import { Script } from 'next/script'
 import '../scss/main.scss'
 
 export const metadata: Metadata = {
@@ -18,6 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-XH5T5WPZ9C" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XH5T5WPZ9C');
+          `}
+        </Script>
+      </head>
       <body className={`${satoshi.variable} antialiased`}>
         <Suspense fallback={<div>Loading...</div>}>
           <SmoothScrollProvider>
