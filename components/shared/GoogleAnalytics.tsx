b7e43ec415fa8a13381c12c2f70af0ad91c185ab
Mirzalazuardi H <mirzalazuardi@gmail.com>
2026-02-28 05:34:51 +0700

    Simplify Google Analytics component

--- components/shared/GoogleAnalytics.tsx
+++ components/shared/GoogleAnalytics.tsx
@@ -1,28 +1,25 @@
 'use client'
 
-import { useEffect } from 'react'
 import Script from 'next/script'
 
 const GA_MEASUREMENT_ID = 'G-XH5T5WPZ9C'
 
 export default function GoogleAnalytics() {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const win = window as unknown as { dataLayer: unknown[]; gtag: Function }
-      win.dataLayer = win.dataLayer || []
-      win.gtag = function () {
-        win.dataLayer.push(arguments)
-      }
-      win.gtag('js', new Date())
-      win.gtag('config', GA_MEASUREMENT_ID)
-    }
-  }, [])
-
   return (
-    <Script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
   )
 }
