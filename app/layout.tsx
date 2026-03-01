1f423174e1d6c7539295b19b977089314e97de07
Mirzalazuardi H <mirzalazuardi@gmail.com>
2026-03-01 23:40:28 +0700

    Optimize: Add SEO metadata, image optimization, LCP fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,28 @@ import { ReactNode, Suspense } from 'react'
 import '../scss/main.scss'
 
 export const metadata: Metadata = {
+  title: {
+    default: 'Metta Legita | Pianist, Composer, Educator',
+    template: '%s | Metta Legita',
+  },
+  description: 'Metta Legita is a pianist, composer, and music educator based in Phnom Penh, Cambodia. Known for jazz-contemporary works and cross-disciplinary projects.',
+  keywords: ['pianist', 'composer', 'music educator', 'jazz', 'Phnom Penh', 'Cambodia'],
+  authors: [{ name: 'Metta Legita' }],
+  openGraph: {
+    title: 'Metta Legita | Pianist, Composer, Educator',
+    description: 'Metta Legita is a pianist, composer, and music educator based in Phnom Penh, Cambodia.',
+    type: 'website',
+    locale: 'en_US',
+    siteName: 'Metta Legita',
+  },
+  twitter: {
+    card: 'summary_large_image',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+}
   title: 'Rivor Template',
 }
 
--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,23 @@ const nextConfig: NextConfig = {
     root: __dirname,
   },
   images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
+      {
+        protocol: 'https',
+        hostname: '*.cloudinary.com',
+      },
     ],
   },
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
 }
 
 export default nextConfig
