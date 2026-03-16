af5f5af196d1bcc593e625493a001ff34778c261
Mirzalazuardi H <mirzalazuardi@gmail.com>
2026-03-17 06:36:16 +0700

    Fix About section button not clickable
    Remove -z-10 from AboutV2 section which was causing
    the button to be behind other elements.

--- components/homepage-03/AboutV2.tsx
+++ components/homepage-03/AboutV2.tsx
@@ -17,7 +17,7 @@ const AboutV2 = ({
   const { revealRef } = useReveal()
 
   return (
-    <section className="relative -z-10 overflow-hidden bg-backgroundBody pb-14 pt-0 dark:bg-dark md:pb-16 lg:pb-[88px] lg:pt-[100px] xl:pb-[100px]">
+    <section className="relative overflow-hidden bg-backgroundBody pb-14 pt-0 dark:bg-dark md:pb-16 lg:pb-[88px] lg:pt-[100px] xl:pb-[100px]">
       <div className="container">
         <div>
           <h3 ref={revealRef} className="text-secondary dark:text-backgroundBody md:text-5xl md:leading-[1.5]">
