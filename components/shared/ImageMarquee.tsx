dd06028ff18b17c6595d6e2420e6de9bc02ea138
Mirzalazuardi H <mirzalazuardi@gmail.com>
2026-03-17 06:19:11 +0700

    Remove old template services, fix ImageMarquee height variance
    - Delete placeholder servicesV2 markdown files
    - Homepage now uses services.items from homepage.json (CMS editable)
    - Add alternating heights to ImageMarquee for staggered effect

--- components/shared/ImageMarquee.tsx
+++ components/shared/ImageMarquee.tsx
@@ -20,31 +20,36 @@ const ImageMarquee = ({ images, speed = 40 }: ImageMarqueeProps) => {
       <RevealWrapper className="marquee-container">
         <Marquee pauseOnHover speed={speed} autoFill>
           <div className="flex items-end gap-3 first:mr-3">
-            {images.map((item) => (
-              <figure key={item.id} className="group relative min-w-56 md:min-w-[362px]">
-                <img src={item.image} alt={item.alt} className="w-full" />
-                {item.link && (
-                  <a
-                    href={item.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="absolute left-1/3 top-[55%] cursor-pointer opacity-0 transition-all duration-[400ms] ease-in-out group-hover:left-1/2 group-hover:top-1/2 group-hover:-translate-x-1/2 group-hover:-translate-y-1/2 group-hover:opacity-100">
-                    <span>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
-                        <rect width="48" height="48" fill="#151515" />
-                        <path
-                          d="M18 30L30 18M30 18H20M30 18V28"
-                          stroke="#EDF0F5"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                    </span>
-                  </a>
-                )}
-              </figure>
-            ))}
+            {images.map((item, index) => {
+              // Alternate heights: odd items are taller
+              const isTall = index % 2 === 0
+              const heightClass = isTall ? 'h-[320px] md:h-[480px]' : 'h-[260px] md:h-[380px]'
+              return (
+                <figure key={item.id} className={`group relative w-56 md:w-[362px] ${heightClass}`}>
+                  <img src={item.image} alt={item.alt} className="h-full w-full object-cover" />
+                  {item.link && (
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="absolute left-1/3 top-[55%] cursor-pointer opacity-0 transition-all duration-[400ms] ease-in-out group-hover:left-1/2 group-hover:top-1/2 group-hover:-translate-x-1/2 group-hover:-translate-y-1/2 group-hover:opacity-100">
+                      <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+                          <rect width="48" height="48" fill="#151515" />
+                          <path
+                            d="M18 30L30 18M30 18H20M30 18V28"
+                            stroke="#EDF0F5"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </span>
+                    </a>
+                  )}
+                </figure>
+              )
+            })}
           </div>
         </Marquee>
       </RevealWrapper>
