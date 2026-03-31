a05b2912a1d8f14ab562bdd646a0ee90cc854d98
Mirzalazuardi H <mirzalazuardi@gmail.com>
2026-04-01 00:43:15 +0700

    remove cta text from about page, keep contact button
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,4 @@
 import CTA from '@/components/shared/CTA'
-import CtaImageSlider from '@/components/shared/CtaImageSlider'
 import LayoutOne from '@/components/shared/LayoutOne'
 import PageHero from '@/components/shared/PageHero'
 
@@ -40,18 +39,7 @@ const AboutPage = () => {
           </div>
         </div>
       </section>
-      <CTA>
-        Let's create
-        <CtaImageSlider
-          slides={[
-            { id: '1', img: '/images/uploads/songs_white_500x200.png' },
-            { id: '2', img: '/images/uploads/harmony_white_500x200.png' },
-            { id: '3', img: '/images/uploads/melodies_white_500x200.png' },
-          ]}
-        />
-        together.
-        <i className="block font-instrument italic max-md:inline-block max-sm:pl-2 sm:mt-10">Music that moves you</i>
-      </CTA>
+      <CTA>{''}</CTA>
     </LayoutOne>
   )
 }
